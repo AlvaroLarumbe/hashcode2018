7002548f024eae9982e2d1f8425e8312615e32a6
Iñaki Garcia <inaki1213@gmail.com>
2018-03-01 19:37:35 +0100

    Vehicle model

---
+++ src/model/Vehicle.js
@@ -0,0 +1,39 @@
+"use strict";
+
+class Vehicle {
+
+    constructor (pos, ride) {
+        this._pos = pos;
+        this._ride = ride;
+    }
+
+    toString () {
+        return `VPos: ${this._pos} | InUse: ${this._in_use}g | EndPos: ${this._end}g `
+    }
+
+    print () {
+        console.log( this.toString() );
+    }
+
+    get pos() {
+        return this._pos;
+    }
+
+    set pos(value) {
+        this._pos = value;
+    }
+
+    get ride() {
+        return this._ride;
+    }
+
+    set ride(value) {
+        this._ride = value;
+    }
+
+    hasRide() {
+        return this._ride !== null;
+    }
+}
+
+module.exports.vehicle = Vehicle;
