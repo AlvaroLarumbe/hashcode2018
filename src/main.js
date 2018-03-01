790df5fa94d1ff2d88fabb30803281cfee01bb16
Iñaki Garcia <inaki1213@gmail.com>
2018-03-01 21:27:08 +0100

    BioPotilPower

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ init.init();
 let city = new City.City('./datasets/a_example.in');
 
 city.nextStep();
-let cont = 1;
 for (var v of city.vehicles) {
+    let cont = 0;
     let rides = "";
     for (var r of v.rides) {
         rides = rides + " " + r;
+        cont++;
     }
-    // console.log(${cont} + " " + ${rides});
-    console.log(`V#: ${cont} | Rides: ${rides}`);
+    console.log(`${cont} ${rides}`);
 }
