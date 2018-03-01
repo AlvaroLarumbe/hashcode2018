83861c668e441277148e659f10b94fcf95e9bd8d
Iñaki Garcia <inaki1213@gmail.com>
2018-03-01 21:59:02 +0100

    Acabaremos partiendo piernas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 const init = require('./init.js');
 const City = require('./model/City.js');
 
 init.init();
 
 let city = new City.City('./datasets/a_example.in');
+let ridesPerVeh = "./datasets/rides_per_vehicle.out";
 
 city.nextStep();
 
@@ -13,8 +15,10 @@ for (var v of city.vehicles) {
     let cont = 0;
     let rides = "";
     for (var r of v.rides) {
-        rides = rides + " " + r;
+        rides = rides + " " + r.index;
         cont++;
     }
-    console.log(`${cont} ${rides}`);
+    rides = cont + " " + rides;
+    console.log(`${rides}`);
+    fs.writeFileSync(ridesPerVeh, ${rides});
 }
