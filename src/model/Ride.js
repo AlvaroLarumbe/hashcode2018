74c690ee158f90e21a0b915d8b4c1e93bbb7eb37
Álvaro Larumbe <biobiotm@gmail.com>
2018-03-01 19:48:10 +0100

    Pusheo antológico

---
+++ src/model/Ride.js
@@ -0,0 +1,44 @@
+export class Ride {
+
+    constructor(start = [0, 0], end, earliestStart, latestFinish) {
+        this._start = start;
+        this._end = end;
+        this._earliestStart = earliestStart;
+        this._latestFinish = latestFinish;
+    }
+
+    get start() {
+        return this._start;
+    }
+
+    set start(value) {
+        this._start = value;
+    }
+
+    get end() {
+        return this._end;
+    }
+
+    set end(value) {
+        this._end = value;
+    }
+
+    get earliestStart() {
+        return this._earliestStart;
+    }
+
+    set earliestStart(value) {
+        this._earliestStart = value;
+    }
+
+    get latestFinish() {
+        return this._latestFinish;
+    }
+
+    set latestFinish(value) {
+        this._latestFinish = value;
+    }
+
+}
+
+module.exports.Ride = Ride;
